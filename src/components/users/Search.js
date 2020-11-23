9cf1b95252cdc503a7a6c0a457f40e71e39f7194
phihuynh94 <phihuynh94@gmail.com>
2020-11-23 00:54:08 -0700

    Update Search.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -24,7 +24,7 @@ const Search = ({ showClear, searchUsers, clearUsers, setAlert }) => {
         <input
           type="text"
           name="text"
-          placeholder="Search Users... test"
+          placeholder="Search Users..."
           value={text}
           onChange={onChange}
         />
